1b6098a95557b12f9e76ca3ff442c51639984389
Lucas Duarte Correia <84639763+lucasz-dc@users.noreply.github.com>
2021-08-19 15:51:04 -0300

    Update ex086.py

--- ex086.py
+++ ex086.py
@@ -1,15 +1,15 @@
 mat = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-for line in range (0, 3):
+for line in range(0, 3):
 
-    for column in range (0, 3):
+    for column in range(0, 3):
         mat[line][column] = int(input(f'Digite um valor para [{line}, {column}]: '))
 
 print('-=-' * 15)
 
-for line in range (0, 3):
+for line in range(0, 3):
 
-    for column in range (0, 3):
+    for column in range(0, 3):
         print(f'[{mat[line][column]:^5}]', end='')
 
     print()
