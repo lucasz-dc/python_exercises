6ea5ead5f2a676d5dd35df174444b695ce37b418
Lucas Duarte Correia <84639763+lucasz-dc@users.noreply.github.com>
2021-08-19 15:52:06 -0300

    Update ex087.py

--- ex087.py
+++ ex087.py
@@ -4,7 +4,7 @@ pairs_sum = bigger = column_sum = 0
 
 for line in range(0, 3):
 
-    for column in range (0, 3):
+    for column in range(0, 3):
         mat[line][column] = int(input(f'Digite um valor para [{line}, {column}]: '))
 
 print('-=-' * 15)
